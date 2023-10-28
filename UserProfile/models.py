7c7cead25e9609aa7dbe280aa01a2f140fb6ffea
axelrori <axelrori204@gmail.com>
2023-10-28 22:06:56 +0700

    fix create profile & models

--- UserProfile/models.py
+++ UserProfile/models.py
@@ -2,7 +2,7 @@ import uuid
 from django.db import models
 from authentication.models import User
 
-app_name = 'profile'
+app_name = 'UserProfile'
 
 # Create your models here.
 class Profile(models.Model):
@@ -11,5 +11,5 @@ class Profile(models.Model):
     username = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     description = models.TextField()
-    favorite_category = models.ManyToManyField('KatalogBuku.Category', related_name='profiles', blank=True)
+    favorite_category = models.ForeignKey('KatalogBuku.Category', related_name='profiles', blank=True, null=True, on_delete=models.CASCADE)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
