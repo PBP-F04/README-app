b95c35e94c876c3172dc06129a61f4b144e4fa2f
mhmdhaekal <muhammad.haekal22@ui.ac.id>
2023-10-28 21:16:41 +0700

    search and katalog

--- UserProfile/urls.py
+++ UserProfile/urls.py
@@ -6,5 +6,5 @@ app_name = 'UserProfile'
 urlpatterns = [
     path('profile/', show_profile, name='show_profile'),
     path('create-profile/', create_profile, name='create_profile'),
-    path('edit-profile/', edit_profile, name='edit_profile'),
+    path('edit-profile/<int:id>/', edit_profile, name='edit_profile'),
 ]
