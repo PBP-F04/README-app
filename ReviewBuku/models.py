c876cd8557af7e0aeb7c284614f235e22ae42266
ayundhasachi <ayundhamulia@gmail.com>
2023-10-28 18:58:23 +0700

    progress urls, views, templates, models

--- ReviewBuku/models.py
+++ ReviewBuku/models.py
@@ -1,3 +1,23 @@
+import uuid
 from django.db import models
 
+from authentication.models import User
+
+app_name = 'review'
 # Create your models here.
+class BookReview(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    book = models.ForeignKey() #sama
+    review_score = models.TextField()
+    review_content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UpvotedReview(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey() #sama
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
--- ReviewBuku/views.py
+++ ReviewBuku/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url='/login/')
+def show_read_books(request):
+    context = {
+
+    }
+
+    return render(request, 'read_books.html', context)
+
+def review_buku(request):
+    context = {
+
+    }
+
+    return render(request, 'review_buku.html', context)
