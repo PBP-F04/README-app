a4fbb1d7cdfc32bd1f14eea1bb7ff74adf672f3f
axelrori <axelrori204@gmail.com>
2023-10-29 18:09:24 +0700

    update css user_profile

--- UserProfile/urls.py
+++ UserProfile/urls.py
@@ -6,6 +6,6 @@ app_name = 'UserProfile'
 urlpatterns = [
     path('', show_profile, name='show_profile'),
     path('create-profile/', create_profile, name='create_profile'),
-    path('edit-profile/<uuid:id>/', edit_profile, name='edit_profile'),
+    path('edit-profile/', edit_profile, name='edit_profile'),
     path('get-categories/', get_categories, name='get_categories'),
 ]
