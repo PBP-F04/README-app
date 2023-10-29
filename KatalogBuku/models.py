73523d51bc0d4718030e8c19367c026266523b55
mhmdhaekal <muhammad.haekal22@ui.ac.id>
2023-10-29 20:53:55 +0700

    update models.py

--- KatalogBuku/models.py
+++ KatalogBuku/models.py
@@ -17,6 +17,9 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'{self.title}'
+
 
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
