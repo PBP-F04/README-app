3024c01a7edc6cacb6e1ad03ad1817e44b61d6f7
mhmdhaekal <muhammad.haekal22@ui.ac.id>
2023-10-31 00:03:01 +0700

    fix urls

--- pinjambuku/urls.py
+++ pinjambuku/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('borrow/<uuid:book_id>/', views.borrow_book, name='borrow_book'),
     path('return/<uuid:loan_id>/', views.return_book, name='return_book'),
     path('track_return_date/<uuid:loan_id>/', views.track_return_date, name='track_return_date'),
-    path('check_book_stock/<uuid:book_id>/', views.check_book_stock, name='check_book_stock'),
-    path('find_book/', views.find_book, name='find_book'),
+    # path('check_book_stock/<uuid:book_id>/', views.check_book_stock, name='check_book_stock'),
+    # path('find_book/', views.find_book, name='find_book'),
 ]
