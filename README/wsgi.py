2596e1779cbc25efadc589d52cc2f2dfc207a67e
mhmdhaekal <muhammad.haekal22@ui.ac.id>
2023-12-12 23:27:11 +0700

    fix deployment

--- README/wsgi.py
+++ README/wsgi.py
@@ -8,16 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "README.settings")
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
 application = get_wsgi_application()
-application = WhiteNoise(application, root=STATIC_ROOT)
