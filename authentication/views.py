35c57caa3f1e226ec6246b7512c010015e16fe29
mhmdhaekal <muhammad.haekal22@ui.ac.id>
2023-12-13 00:15:12 +0700

    fix deployment

--- authentication/views.py
+++ authentication/views.py
@@ -14,85 +14,95 @@ from django.views.decorators.csrf import csrf_exempt
 def user_login(request):
     if request.method == "POST":
         body = json.loads(request.body)
-        email = body['email']
-        password = body['password']
-        user = authenticate(request, username=email,
-                            password=password)
+        email = body["email"]
+        password = body["password"]
+        user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
-            response = {
-                'status': 200,
-                'message': 'Login success'
-            }
-            return HttpResponse(json.dumps(response), content_type='application/json', status=200)
+            response = {"status": 200, "message": "Login success"}
+            return HttpResponse(
+                json.dumps(response), content_type="application/json", status=200
+            )
         else:
-            return HttpResponse(json.dumps({'status': 400, 'message': 'Incorrect Email and Password'}),
-                                content_type='application/json',
-                                status=400)
+            return HttpResponse(
+                json.dumps({"status": 400, "message": "Incorrect Email and Password"}),
+                content_type="application/json",
+                status=400,
+            )
 
-    return render(request, 'index.html')
+    return render(request, "index.html")
 
 
+@csrf_exempt
 def user_register(request):
     if request.method == "POST":
         body = json.loads(request.body)
-        email = body['email']
-        password = body['password']
-        confirm_password = body['confirmPassword']
+        email = body["email"]
+        password = body["password"]
+        confirm_password = body["confirmPassword"]
 
         try:
             validate_email(email)
         except ValidationError as e:
             response = {
-                'status': 400,
-                'message': 'Email is not valid',
-                'errorCode': 402
+                "status": 400,
+                "message": "Email is not valid",
+                "errorCode": 402,
             }
-            return HttpResponse(json.dumps(response), content_type='application/json', status=400)
+            return HttpResponse(
+                json.dumps(response), content_type="application/json", status=400
+            )
 
         if len(email) == 0 or len(password) == 0 or len(confirm_password) == 0:
             response = {
-                'status': 400,
-                'message': 'Username, Password and Confirm Password cannot be empty',
-                'errorCode': 400
+                "status": 400,
+                "message": "Username, Password and Confirm Password cannot be empty",
+                "errorCode": 400,
             }
-            return HttpResponse(json.dumps(response), content_type='application/json', status=400)
+            return HttpResponse(
+                json.dumps(response), content_type="application/json", status=400
+            )
 
         if len(password) < 8:
             response = {
-                'status': 400,
-                'message': 'Password must be at least 8 characters',
-                'errorCode': 400
+                "status": 400,
+                "message": "Password must be at least 8 characters",
+                "errorCode": 400,
             }
-            return HttpResponse(json.dumps(response), content_type='application/json', status=400)
+            return HttpResponse(
+                json.dumps(response), content_type="application/json", status=400
+            )
 
         if password != confirm_password:
             response = {
-                'status': 400,
-                'message': 'Password and Confirm Password not match',
-                'errorCode': 400
+                "status": 400,
+                "message": "Password and Confirm Password not match",
+                "errorCode": 400,
             }
-            return HttpResponse(json.dumps(response), content_type='application/json', status=400)
+            return HttpResponse(
+                json.dumps(response), content_type="application/json", status=400
+            )
 
         if User.objects.filter(username=email).exists():
             response = {
-                'status': 400,
-                'message': 'Email already exists',
-                'errorCode': 401
+                "status": 400,
+                "message": "Email already exists",
+                "errorCode": 401,
             }
-            return HttpResponse(json.dumps(response), content_type='application/json', status=400)
+            return HttpResponse(
+                json.dumps(response), content_type="application/json", status=400
+            )
 
         user = User.objects.create_user(username=email, password=password)
         user.save()
-        response = {
-            'status': 200,
-            'message': 'Register success'
-        }
-        return HttpResponse(json.dumps(response), content_type='application/json', status=200)
+        response = {"status": 200, "message": "Register success"}
+        return HttpResponse(
+            json.dumps(response), content_type="application/json", status=200
+        )
 
-    return render(request, 'register.html')
+    return render(request, "register.html")
 
 
 def user_logout(request):
     logout(request)
-    return HttpResponseRedirect(reverse('authentication:login'))
+    return HttpResponseRedirect(reverse("authentication:login"))
